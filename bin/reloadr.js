1f92f4ca4f894dc2413b4d9b79643f0014e3b419
Greg Allen <me@jga.me>
2012-12-12 13:54:45 -0800

    feature[bin] added --help and --version

--- bin/reloadr.js
+++ bin/reloadr.js
@@ -1,13 +1,34 @@
 #!/usr/bin/env node
 
+var fs = require('fs');
+var version = JSON.parse(fs.readFileSync(__dirname + '/../package.json', 'utf8')).version
 
 var reloadr = require('../lib/reloadr');
 
-var folders = [];
-for (var i = 2, c = process.argv.length; i < c; i++) {
-  var item = process.argv[i];
-  folders.push(item);
+var opt = require('optimist')
+    .usage('Reloadr '+ version +'\nUsage: $0 [dirs]')
+    .options('v', {
+      alias: 'version',
+      descripe: 'Show version'
+    })
+    .options('h', {
+      alias: 'help',
+      descripe: 'Show help info'
+    });
+
+var argv = opt.argv;
+
+if (argv.version) {
+  return console.log(version);
 }
+if (argv.help) {
+  return opt.showHelp(function(help) {
+    console.log(help);
+    console.log('Example dirs: example/*.js example/* example/**/*');
+  });
+}
+
+var folders = argv._;
 
 if (folders.length == 0) {
   folders.push('**/*');
